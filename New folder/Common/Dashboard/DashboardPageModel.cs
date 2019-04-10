7534a46b72e738735cba71920103b0ada40923f9
TDS DEMO <tdsdemo@tdt.local>
2019-04-10 11:01:36 +0700

    sd

---
+++ New folder/Common/Dashboard/DashboardPageModel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace COCOSIN_ERP.Common
+{
+    public class DashboardPageModel
+    {
+        public int OpenOrders { get; set; }
+        public int ClosedOrderPercent { get; set; }
+        public int CustomerCount { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
---
+++ New folder/Common/ScriptInitialization.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="../Common/Helpers/LanguageList.ts" />
+
+namespace COCOSIN_ERP.ScriptInitialization {
+    Q.Config.responsiveDialogs = true;
+    Q.Config.rootNamespaces.push('COCOSIN_ERP');
+    Q.Config.rootNamespaces.push('_Ext');
+    Serenity.EntityDialog.defaultLanguageList = LanguageList.getValue;
+
+    if ($.fn['colorbox']) {
+        $.fn['colorbox'].settings.maxWidth = "95%";
+        $.fn['colorbox'].settings.maxHeight = "95%";
+    }
+
+    window.onerror = Q.ErrorHandling.runtimeErrorHandler;
+}   
