00c95011feae2dbfccbf310395be731c3e5d4c4c
qthinhqn <45504087+qthinhqn@users.noreply.github.com>
2019-05-08 23:07:51 +0700

    report

---
+++ CanteenTHP/Modules/CantinTHP/Reports/NewReport.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Canteen.CantinTHP
+{
+    using Entities;
+    using Serenity.ComponentModel;
+    using Serenity.Data;
+    using Serenity.Reporting;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Drawing;
+
+    //[Report, RequiredPermission(PermissionKeys.General)]
+    [Category("CantinTHP"), DisplayName("New Report")]
+    public class NewReportReport : IReport, IDataOnlyReport
+    {
+        [DisplayName("Start Date")]
+        public DateTime? StartDate { get; set; }
+
+        [DisplayName("End Date")]
+        public DateTime? EndDate { get; set; }
+        
+
+        public object GetData()
+        {
+            using (var connection = SqlConnections.NewFor<Entities.TbEmployeeRow>())
+            {
+                return connection.Query<Item>("spEmployeeList",
+                    param: new
+                    {
+                        startDate = StartDate,
+                        endDate = EndDate
+                    },
+                    commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+
+        public List<ReportColumn> GetColumnList()
+        {
+            return ReportColumnConverter.ObjectTypeToList(typeof(Item));
+        }
+
+        [BasedOnRow(typeof(TbEmployeeRow), CheckNames = true)]
+        public class Item
+        {
+            public String EmployeeId { get; set; }
+            public String EmployeeName { get; set; }
+            public Boolean Active { get; set; }
+            public DateTime LeftDate { get; set; }
+        }
+        
+    }
+}
---
+++ CanteenTHP/Modules/CantinTHP/Reports/ReportsPage.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Canteen.CantinTHP.Pages
+{
+    using Canteen;
+    using System.Web.Mvc;
+
+    [RoutePrefix("CantinTHP/Reports"), Route("{action=index}")]
+    //[PageAuthorize(PermissionKeys.)]
+    public class ReportsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(MVC.Views.Common.Reporting.ReportPage, 
+                new ReportRepository().GetReportTree("CantinTHP"));
+        }
+    }
+}
